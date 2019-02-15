8763f3bf107020bbc71585c05ef289bff83a101e
philexohf <philex_hf@163.com>
2019-02-15 20:02:14 +0800

    视频的canny边缘检测

--- 2_canny_video.py
+++ 2_canny_video.py
@@ -1,13 +1,13 @@
 import cv2
 
-# 获得视频的格式
+
 videoCapture = cv2.VideoCapture('ow_reunion.mp4')
 
 # 获得码率及尺寸
 fps = videoCapture.get(cv2.CAP_PROP_FPS)
 size = (int(videoCapture.get(cv2.CAP_PROP_FRAME_WIDTH)),
         int(videoCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-fNUMS = videoCapture.get(cv2.CAP_PROP_FRAME_COUNT)
+
 print('fps={}, size={}'.format(fps, size))
 
 saveImage = cv2.VideoWriter_fourcc(*'XVID')
@@ -21,7 +21,7 @@ while success:
     cv2.imshow('Video', frame)  # 显示
     frame = cv2.GaussianBlur(frame, (3, 3), 0)
     grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cannyImage = cv2.Canny(grayImage, 30, 150)
+    cannyImage = cv2.Canny(grayImage, 50, 150)
     dst = cv2.bitwise_and(frame, frame, mask=cannyImage)
     cv2.imshow('dstVideo', dst)
     out.write(dst)
