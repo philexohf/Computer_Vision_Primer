1848bfd6d2efa4648af8a265176db638c058c9c1
philexohf <philex_hf@163.com>
2019-02-17 02:22:57 +0800

    Update 2_canny_video.py

--- 2_canny_video.py
+++ 2_canny_video.py
@@ -2,7 +2,6 @@ import cv2
 
 
 videoCapture = cv2.VideoCapture('ow_reunion.mp4')
-
 # 获得码率及尺寸
 fps = videoCapture.get(cv2.CAP_PROP_FPS)
 size = (int(videoCapture.get(cv2.CAP_PROP_FRAME_WIDTH)),
@@ -18,14 +17,14 @@ cv2.namedWindow('dstVideo', 0)
 # 读帧
 success, frame = videoCapture.read()
 while success:
-    cv2.imshow('Video', frame)  # 显示
+    cv2.imshow('Video', frame)
     frame = cv2.GaussianBlur(frame, (3, 3), 0)
     grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cannyImage = cv2.Canny(grayImage, 50, 150)
     dst = cv2.bitwise_and(frame, frame, mask=cannyImage)
     cv2.imshow('dstVideo', dst)
     out.write(dst)
-    qKey = cv2.waitKey(int(1000 / fps))  # 延迟
+    qKey = cv2.waitKey(int(1000 / fps))
     if qKey == (ord('q') or ord('Q')):
         break
     success, frame = videoCapture.read()  # 获取下一帧
