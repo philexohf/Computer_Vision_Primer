55eb4242468a0f2464a8638d2d4baabe006cb630
philexohf <philex_hf@163.com>
2019-02-17 00:23:19 +0800

    Update 1_open_image.py

--- 1_open_image.py
+++ 1_open_image.py
@@ -1,9 +1,9 @@
 import cv2
 
 
-img = cv2.imread('earthlight.jpg')  # 读取当前目录下的图片.
+img = cv2.imread('earth_light.jpg')  # 读取当前目录下的图片.
 # path = "C:/Users/Administrator/Pictures/"
-# img = cv2.imread(path + 'earthlight.jpg')
+# img = cv2.imread(path + 'earth_light.jpg')
 cv2.namedWindow('img', 0)  # 创建一个窗口，参数0表示窗口大小可改变.
 cv2.imshow('img', img)  # 显示图片，第一个参数是显示图像的窗口的名字，第二个参数是要显示的图像.
 
