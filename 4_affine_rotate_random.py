b48d3053ecb678311430466be09485d4bf0ad559
philexohf <philex_hf@163.com>
2019-02-23 20:36:36 +0800

    2019-2-23

--- 4_affine_rotate_random.py
+++ 4_affine_rotate_random.py
@@ -2,23 +2,21 @@ import cv2
 import numpy as np
 import random
 
-img = cv2.imread('hnj.png')
-imgx = cv2.imread('ev.jpg')
-cv2.imshow('yste', img)
+srcImg = cv2.imread('xs.jpg')
+bgImg = cv2.imread('earth_light.jpg')
+cv2.namedWindow('dst', 0)
 
 while True:
-    i = 0
-    while i <= 360:
+    for i in range(360):
         theta = i * np.pi / 180
         M_rotate = np.array([
             [np.cos(theta), -np.sin(theta), 500 - 10*random.randint(1,10)],
             [np.sin(theta), np.cos(theta), 500 - 10*random.randint(1,10)]
         ], dtype=np.float32)
 
-        img_rotated = cv2.warpAffine(img, M_rotate, (1080, 1080))
-        dst = cv2.addWeighted(img_rotated, 0.7, imgx, 0.3, 0)
-        cv2.imshow('yste', dst)
-        i += 1
-        cv2.waitKey(30)
+        img_rotated = cv2.warpAffine(srcImg, M_rotate, (1080, 1080))
+        dst = cv2.addWeighted(img_rotated, 0.7, bgImg, 0.3, 0)
+        cv2.imshow('dst', dst)
+        cv2.waitKey(25)
 
 cv2.destroyAllWindows()
