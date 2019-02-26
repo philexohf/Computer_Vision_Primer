352b09b4e54206a2ff07f17cc352974404f8a692
philexohf <philex_hf@163.com>
2019-02-26 23:08:35 +0800

    2019-2-26

--- 1_open_camera.py
+++ 1_open_camera.py
@@ -1,18 +1,18 @@
 import cv2
 
 
-videoCapture = cv2.VideoCapture(0)
-size = (int(videoCapture.get(cv2.CAP_PROP_FRAME_WIDTH)),
-        int(videoCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+cap = cv2.VideoCapture(0)
+size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 print('size={}'.format(size))
 cv2.namedWindow('camera', 0)
 # 读帧
-success, frame = videoCapture.read()
+success, frame = cap.read()
 while success:
     cv2.imshow('camera', frame)  # 显示
-    qKey = cv2.waitKey(int(1000 / 30))  # 延迟
-    if qKey == (ord('q') or ord('Q')):
+    quitKey = cv2.waitKey(int(1000 / 30))  # 延迟
+    if quitKey == (ord('q') or ord('Q')):
         break
-    success, frame = videoCapture.read()  # 获取下一帧
+    success, frame = cap.read()  # 获取下一帧
 
-videoCapture.release()
+cap.release()
