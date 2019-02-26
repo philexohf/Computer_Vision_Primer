162d46987bf4ce878df1a99245a36cd40fe4f92a
philexohf <philex_hf@163.com>
2019-02-26 23:23:29 +0800

    2019-2-26

--- 1_open_video.py
+++ 1_open_video.py
@@ -1,22 +1,20 @@
 import cv2
 
-
-videoCapture = cv2.VideoCapture('ow_reunion.mp4')
-
+path = "D:/PycharmProjects/Test/"  # 视频文件存放的绝对路径
+cap = cv2.VideoCapture(path + 'ow_reunion.mp4')
 # 获得码率及尺寸
-fps = videoCapture.get(cv2.CAP_PROP_FPS)
-size = (int(videoCapture.get(cv2.CAP_PROP_FRAME_WIDTH)),
-        int(videoCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-
+fps = cap.get(cv2.CAP_PROP_FPS)
+size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 print('fps={}, size={}'.format(fps, size))
-cv2.namedWindow('mp4', 0)
-# 读帧
-success, frame = videoCapture.read()
-while success:
-    cv2.imshow('mp4', frame)  # 显示
-    qKey = cv2.waitKey(int(1000 / fps))  # 延迟
-    if qKey == (ord('q') or ord('Q')):
-        break
-    success, frame = videoCapture.read()  # 获取下一帧
+cv2.namedWindow('testVideo', 0)
 
-videoCapture.release()
+success, frame = cap.read()
+while success:
+    cv2.imshow('testVideo', frame)
+    quitKey = cv2.waitKey(int(1000 / fps))
+    if quitKey == (ord('q') or ord('Q')):
+        break
+    success, frame = cap.read()
+
+cap.release()
