3d9db8e0df98ba6937e849aea4ac1e11e86adb01
philexohf <philex_hf@163.com>
2019-03-06 12:16:26 +0800

    2019-3-6

---
+++ 1_channel_image.py
@@ -0,0 +1,12 @@
+import cv2
+
+
+img = cv2.imread("summer.png")
+cv2.imshow('src', img)
+# img[:, :, 2] = 0  # 红色通道置零
+img[250:600, 200:400, 1] = 0  # 区域(高：250-600，宽：200-400)绿色通道置零
+# img[:, :, 0] = 0  # 蓝色通道置零
+cv2.imshow('dst', img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
