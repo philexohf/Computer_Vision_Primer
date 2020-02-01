ea449a2031d6e1ceab8a64a26b16e4eeb8c18b0d
philexohf <philex_hf@163.com>
2020-02-01 19:10:05 +0800

    2020.2.1

---
+++ 7_HOG_image.py
@@ -0,0 +1,34 @@
+import cv2
+
+img = cv2.imread('./image/mercy2.jpg')
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+found, w = hog.detectMultiScale(img)
+
+
+def is_inside(o, i):
+    ox, oy, ow, oh = o
+    ix, iy, iw, ih = i
+    return ox > ix and oy > iy and ox + ow < ix + iw and oy + oh < iy + ih
+
+
+def draw_person(image, person):
+    x, y, w, h = person
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+
+found_filter = []
+for ri, r in enumerate(found):
+    for qi, q in enumerate(found):
+        if ri != qi and is_inside(r, q):
+            break
+        else:
+            found_filter.append(r)
+
+for person in found_filter:
+    draw_person(img, person)
+
+cv2.namedWindow('person_detector', 0)
+cv2.imshow('person_detector', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
