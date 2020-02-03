ae5ae17aa16f2036d2d23400d79a08351fd8c45f
philexohf <philex_hf@163.com>
2020-02-03 14:45:30 +0800

    2020.2.3

--- 7_HoughCircles_image.py
+++ 7_HoughCircles_image.py
@@ -6,14 +6,15 @@ import numpy as np
 img = cv2.imread('./image/houghcircles.jpg')
 grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 grayImg = cv2.medianBlur(grayImg, 5)
+color = (0, 0, 255)
 
 houghCircle = cv2.HoughCircles(grayImg, cv2.HOUGH_GRADIENT, 1, 120,
                                param1=100, param2=30, minRadius=0, maxRadius=0)
 houghCircle = np.uint16(np.around(houghCircle))
 
 for i in houghCircle[0, :]:
-    cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)  # 画外圆
-    cv2.circle(img, (i[0], i[1]), 2, (0, 255, 0), 3)  # 画圆心
+    cv2.circle(img, (i[0], i[1]), i[2], color, 2)  # 画外圆
+    cv2.circle(img, (i[0], i[1]), 2, color, 3)  # 画圆心
 
 cv2.namedWindow('HoughCirles', 0)
 cv2.imshow('HoughCirles', img)
--- 7_HoughLinesP_image.py
+++ 7_HoughLinesP_image.py
@@ -3,15 +3,16 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('./image/houghlines.jpg')
+img = cv2.imread('./image/houghlines.png')
 grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-edgeImg = cv2.Canny(grayImg, 50, 120)
-minLineLength = 10
+edgeImg = cv2.Canny(grayImg, 50, 150)
+minLineLength = 20
 maxLineGap = 5
+color = (0, 0, 255)
 houghLine = cv2.HoughLinesP(edgeImg, 1, np.pi/180, 100, minLineLength, maxLineGap)
 
 for x1, y1, x2, y2 in houghLine[0]:
-    cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    cv2.line(img, (x1, y1), (x2, y2), color, 2)
 
 cv2.namedWindow('edges', 0)
 cv2.namedWindow('HoughLines', 0)
