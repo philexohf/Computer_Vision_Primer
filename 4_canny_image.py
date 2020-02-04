a0ab06466f18852798d6a59af56f1c4d9be60e35
philexohf <philex_hf@163.com>
2020-02-04 16:58:03 +0800

    2020.2.4

--- 4_canny_image.py
+++ 4_canny_image.py
@@ -2,7 +2,7 @@
 # https://github.com/philexohf/Computer_Vision_Primer
 import cv2
 
-img = cv2.imread("./image/tamamonomae1.png")
+img = cv2.imread("./image/raspberry.jpg")
 cv2.namedWindow('srcImg')
 cv2.namedWindow('cannyImage')
 
@@ -11,7 +11,7 @@ grayImage = cv2.cvtColor(blurImage, cv2.COLOR_BGR2GRAY)
 cannyImage = cv2.Canny(grayImage, 50, 150)
 cv2.imshow('srcImg', img)
 cv2.imshow("cannyImage", cannyImage)
-# cv2.imwrite("cannyImage.jpg", cannyImage)
+cv2.imwrite("cannyImage.jpg", cannyImage)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
