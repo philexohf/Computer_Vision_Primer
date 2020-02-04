878e021ad834b4de5d85d6d39b48f9db42045508
philexohf <philex_hf@163.com>
2020-02-05 01:50:01 +0800

    2020.2.5

--- 1_rw_camera.py
+++ 1_rw_camera.py
@@ -4,26 +4,26 @@ import cv2
 
 cap = cv2.VideoCapture(0)  # 获取摄像头帧流，参数0为摄像头设备索引号
 
-fps = 30  # 因为get()方法不能返回摄像头的帧率，所以设置为固定值30
-# get()方法获取摄像头帧的尺寸
+# get()方法获取摄像头帧率和图像尺寸
+fps = int(cap.get(cv2.CAP_PROP_FPS))
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 size = (width, height)
-print('size={}'.format(size))
+print(f'fps = {fps}, size={size}')  # f格式适用于Python3.6.8以上，3.6.8以下版本请使用format格式
 
 fourcc = cv2.VideoWriter_fourcc(*'XVID')  # 指定视频编解码器
 out = cv2.VideoWriter('myCamera.avi', fourcc, fps, size)  # 为构造函数指定视频文件名称
-cv2.namedWindow('camera', 0)
-# 读帧
+cv2.namedWindow('Camera', 0)
+# read()方法获取视频帧
 success, frame = cap.read()
 
 while success:    
-    cv2.imshow('camera', frame)  # 显示
+    cv2.imshow('Camera', frame)
     out.write(frame)
-    quitKey = cv2.waitKey(int(1000 / fps))  # 延迟
+    quitKey = cv2.waitKey(int(1000 / fps))
     if quitKey == (ord('q') or ord('Q')):
         break
-    success, frame = cap.read()  # read()方法获取视频帧
+    success, frame = cap.read()
 
-cv2.destroyWindow('camera')
+cv2.destroyWindow('Camera')
 cap.release()
--- 2_channel_image.py
+++ 2_channel_image.py
@@ -1,13 +1,19 @@
 # ===程序功能：BGR颜色通道的操作=== #
 # https://github.com/philexohf/Computer_Vision_Primer
 import cv2
+import numpy as np
 
 img = cv2.imread("./image/summer.png")
-cv2.imshow('src', img)
-# img[:, :, 2] = 0  # 红色通道置零
-img[250:600, 200:400, 1] = 0  # 区域(高：250-600，宽：200-400)绿色通道置零
-# img[:, :, 0] = 0  # 蓝色通道置零
-cv2.imshow('dst', img)
+dstImg = img.copy()  # 获取img图像数据的copy
+
+# dstImg[:, :, 2] = 0  # 红色通道置零
+dstImg[:, :, 1] = 0  # 绿色通道置零
+# dstImg[:, :, 0] = 0  # 蓝色通道置零
+# dstImg[250:600, 200:400, 1] = 0  # 区域(高：250-600，宽：200-400)绿色通道置零
+dst = np.hstack([img, dstImg])  # 将img和dstImg两幅图片合并在一起
+
+cv2.namedWindow('BGR-channel', 0)
+cv2.imshow('BGR-channel', dst)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
